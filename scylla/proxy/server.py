fa2a01959aa3a3e813afb3f6bf449ae62f3630f0
Simon Berger <siku2@outlook.com>
2018-11-21 19:35:03 +0100

    adding https server

--- scylla/proxy/server.py
+++ scylla/proxy/server.py
@@ -1,9 +1,10 @@
 import random
+import socket
 import sys
 import traceback
 from multiprocessing import Process
 
-from tornado import httpclient, web, ioloop
+from tornado import httpclient, web, ioloop, iostream
 from tornado.httpclient import HTTPResponse
 
 from scylla.config import get_config
@@ -26,11 +27,11 @@ def get_proxy(https=False) -> ProxyIP:
     return proxy
 
 
-# TODO: handle https requests
 class ForwardingRequestHandler(web.RequestHandler):
     """
     A very rough ForwardingRequestHandler, only supports HTTP requests.
     """
+    SUPPORTED_METHODS = ['GET', 'POST', 'CONNECT']
 
     def data_received(self, chunk):
         pass
@@ -99,6 +100,65 @@ class ForwardingRequestHandler(web.RequestHandler):
             self.write("Internal server error:\n" +
                        ''.join(traceback.format_exception(*sys.exc_info())))
             self.finish()
+
+    @web.asynchronous
+    def connect(self):
+        logger.debug('Start CONNECT to %s', self.request.uri)
+        host, port = self.request.uri.split(':')
+        client = self.request.connection.stream
+
+        def read_from_client(data):
+            upstream.write(data)
+
+        def read_from_upstream(data):
+            client.write(data)
+
+        def client_close(data=None):
+            if upstream.closed():
+                return
+            if data:
+                upstream.write(data)
+            upstream.close()
+
+        def upstream_close(data=None):
+            if client.closed():
+                return
+            if data:
+                client.write(data)
+            client.close()
+
+        def start_tunnel():
+            logger.debug('CONNECT tunnel established to %s', self.request.uri)
+            client.read_until_close(client_close, read_from_client)
+            upstream.read_until_close(upstream_close, read_from_upstream)
+            client.write(b'HTTP/1.0 200 Connection established\r\n\r\n')
+
+        def on_proxy_response(data=None):
+            if data:
+                first_line = data.splitlines()[0]
+                http_v, status, text = first_line.split(None, 2)
+                if int(status) == 200:
+                    logger.debug('Connected to upstream proxy %s', proxy)
+                    start_tunnel()
+                    return
+
+            self.set_status(500)
+            self.finish()
+
+        def start_proxy_tunnel():
+            upstream.write(b'CONNECT %s HTTP/1.1\r\n' % self.request.uri.encode())
+            upstream.write(b'Host: %s\r\n' % self.request.uri.encode())
+            upstream.write(b'Proxy-Connection: Keep-Alive\r\n\r\n')
+            upstream.read_until(b'\r\n\r\n', on_proxy_response)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        upstream = iostream.IOStream(s)
+
+        proxy = get_proxy(self.request.uri.startswith('https'))
+        if proxy:
+            upstream.connect((proxy.ip, proxy.port), start_proxy_tunnel)
+        else:
+            upstream.connect((host, int(port)), start_tunnel)
 
 
 def make_app():
